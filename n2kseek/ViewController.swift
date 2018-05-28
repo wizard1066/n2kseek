cd029f288ebee9292a29a1b036296dff5c74b574
marklucking <mark.lucking@gmail.com>
2018-05-28 10:43:57 +0200

    Initial Commit

---
+++ n2kseek/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  n2kseek
+//
+//  Created by localuser on 28.05.18.
+//  Copyright © 2018 cqd.ch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
